1e71fc24d50235a2bab39f9db762d2c07e4b40d0
Phil Kunz <gitlab@philkunz.com>
2019-10-12 15:57:43 +0200

    fix(core): update

---
+++ ts/tswatch.classes.elementserver.ts
@@ -0,0 +1,12 @@
+import * as plugins from './tswatch.plugins';
+
+/**
+ * an element server for developing lit elements compliant to gitzone standard
+ */
+export class ElementServer {
+  constructor() {
+
+  }
+
+
+}
